a3c1a9d6a664e2041c30d0c11de73786a00399a6
ThanhPhongsof <ttphongletter@gmail.com>
2022-07-19 22:49:21 +0700

    update UI sign-up

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -2,20 +2,14 @@ import React from "react";
 import { useController } from "react-hook-form";
 import PropTypes from "prop-types";
 
-const Input = ({
-  control,
-  name,
-  placeholder = "",
-  type = "text",
-  className = "",
-}) => {
+const Input = ({ control, name, type = "text", className = "", ...rest }) => {
   const { field } = useController({ control, name, defaultValue: "" });
   return (
     <div className="relative">
       <input
         type={type}
-        placeholder={placeholder}
         className={`w-full px-6 py-4 text-sm font-medium border border-strock rounded-xl text-text1 placeholder:text-text4 ${className}`}
+        {...rest}
         {...field}
       />
     </div>
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import { Input } from "../components/input";
-import { Label } from "../components/label";
-import LayoutAuthentication from "../layout/LayoutAuthentication";
+import { Input } from "components/input";
+import { Label } from "components/label";
+import LayoutAuthentication from "layout/LayoutAuthentication";
+import FormGroup from "components/common/FormGroup";
+import { Button } from "components/button";
+import { Checkbox } from "components/checkbox";
 
 const SignUpPage = () => {
-  const { handleSubmit, control } = useForm();
+  const {
+    handleSubmit,
+    control,
+    formState: { isValid, isSubmitting },
+  } = useForm();
+  const handleSignUp = (values) => {
+    console.log(
+      "🚀 ~ file: SignUpPage.js ~ line 17 ~ handleSignUp ~ values",
+      values
+    );
+  };
+  const [acceptTerm, setAcceptTerm] = useState(false);
+  const handleToggleTerm = () => {
+    setAcceptTerm(!acceptTerm);
+  };
+
   return (
     <LayoutAuthentication heading="Sign Up">
       <p className="mb-6 text-xs font-normal text-center lg:mb-8 lg:text-sm text-text3">
@@ -22,11 +40,44 @@ const SignUpPage = () => {
       <p className="mb-4 text-xs font-normal text-center lg:text-sm lg:mb-8 text-text2">
         Or sign up with email
       </p>
-      <form autoComplete="off">
-        <div className="flex flex-col gap-y-3">
-          <Label>Full Name *</Label>
-          <Input control={control} name="name" placeholder=""></Input>
+      <form autoComplete="off" onSubmit={handleSubmit(handleSignUp)}>
+        <FormGroup>
+          <Label htmlFor="fullname">Full Name *</Label>
+          <Input control={control} name="name" placeholder="Jhon Doe"></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="email">Email *</Label>
+          <Input
+            control={control}
+            name="email"
+            type="email"
+            placeholder="example@gmail.com"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="password">Password *</Label>
+          <Input
+            control={control}
+            name="password"
+            type="password"
+            placeholder="Create a password"
+          ></Input>
+        </FormGroup>
+        <div className="mb-5">
+          <Checkbox name="term" checked={acceptTerm} onClick={handleToggleTerm}>
+            <p className="text-sm text-text2">
+              I agree to the{" "}
+              <span className="text-secondary underline flex-1">
+                Terms of Use
+              </span>{" "}
+              and have read and understand the{" "}
+              <span className="text-secondary underline">Privacy policy.</span>
+            </p>
+          </Checkbox>
         </div>
+        <Button type="submit" className="bg-primary w-full">
+          Create my account
+        </Button>
       </form>
     </LayoutAuthentication>
   );
